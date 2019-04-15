d7092a114b7c84342041612e7a30560712de4ec9
Pedro Pinho <rihor.pedro@gmail.com>
2019-04-15 17:36:52 -0300

    post by json

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {  
-	res.render('index');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.post('/', (req, res) => {
-  res.render('index');
-  console.log(req.body.searchContent);
+  let searchText = req.body.value;
+  console.log(searchText);
 });
 
 app.listen(3333);
