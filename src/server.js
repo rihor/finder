74dd90b9f3d007b2f900d40fa5e07989e798b303
Pedro Pinho <rihor1000@gmail.com>
2020-12-22 10:33:43 -0300

    Atualizando e removendo pacote desnecessário

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
+require("dotenv/config")
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(express.json());
 app.set('view engine', 'ejs');
 // define a pasta onde está as views
 app.set('views', path.join(__dirname, '../public/views'));
